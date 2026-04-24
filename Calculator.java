d35bd5a1cde6264cb6ec8687ad614792ecf7c437
mohithsk05 <mohithsk2005@gmail.com>
2026-04-24 15:35:28 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,21 @@
 public class Calculator {
+    public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
     public static void main(String[] args) {
         Calculator calc = new Calculator();
 
